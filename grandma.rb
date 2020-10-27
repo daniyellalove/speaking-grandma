feb749852464d70607ddcf9e0b0944965ba1eba6
github username <github email address>
2020-10-27 16:29:10 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 string = "I LOVE YOU GRANDMA!"
 def speak_to_grandma(string)
-    if string == "I LOVE YOU GRANDMA!"
-        "I LOVE YOU TOO PUMPKIN!"
-    elsif string == string.downcase
-        "HUH?! SPEAK UP, SONNY!"
+    if string = "I LOVE YOU GRANDMA!"
+       return "I LOVE YOU TOO PUMPKIN!"
+    elsif string = string.downcase
+        return "HUH?! SPEAK UP, SONNY!"
     else 
-        "NO, NOT SINCE 1938!" 
+        return "NO, NOT SINCE 1938!" 
     end
 end
 # Write a speak_to_grandma method.
